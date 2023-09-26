3af76a70b6ea0d6b5a2211f76710993821bcd832
octaprog7 <goctaprog@gmail.com>
2023-09-26 21:18:24 +0500

    первый

---
+++ main.py
@@ -0,0 +1,68 @@
+# MicroPython
+# mail: goctaprog@gmail.com
+# MIT license
+# import time
+# Пожалуйста, прочитайте документацию на QMC5883L!
+# Please read the QMC5883L documentation!
+import math
+from machine import I2C, Pin
+
+import qmc5883mod
+import time
+from sensor_pack.bus_service import I2cAdapter
+
+
+if __name__ == '__main__':
+    # пожалуйста установите выводы scl и sda в конструкторе для вашей платы, иначе ничего не заработает!
+    # please set scl and sda pins for your board, otherwise nothing will work!
+    # https://docs.micropython.org/en/latest/library/machine.I2C.html#machine-i2c
+    # i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=400_000) # для примера
+    # bus =  I2C(scl=Pin(4), sda=Pin(5), freq=100000)   # на esp8266    !
+    # Внимание!!!
+    # Замените id=1 на id=0, если пользуетесь первым портом I2C !!!
+    # Warning!!!
+    # Replace id=1 with id=0 if you are using the first I2C port !!!
+    # i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=400_000) # для примера
+
+    # i2c = I2C(id=1, scl=Pin(27), sda=Pin(26), freq=400_000)  # on Arduino Nano RP2040 Connect and Pico W tested!
+    i2c = I2C(id=1, scl=Pin(7), sda=Pin(6), freq=400_000)  # create I2C peripheral at frequency of 400kHz
+    adapter = I2cAdapter(i2c)  # адаптер для стандартного доступа к шине
+    delay_func = time.sleep_ms
+
+    sensor = qmc5883mod.QMC5883L(adapter)
+    print(f"Sensor id: {sensor.get_id()}")
+    print(16 * "_")
+
+    upd_rate = 0
+    print("Continuous meas mode measurement. 2 Milli-Gauss Field Resolution!")
+    sensor.start_measure(continuous_mode=True, update_rate=upd_rate, full_scale=False, over_sample_ratio=3)
+    print(f"Is continuous meas mode: {sensor.is_continuous_meas_mode()}")
+    wt = sensor.get_conversion_cycle_time()
+    delay_func(wt)
+    index = 0
+    for mf_comp in sensor:
+        delay_func(wt)
+        if mf_comp:
+            x = math.sqrt(sum(map(lambda val: val ** 2, mf_comp)))  # напряженность магнитного поля в условных ед.
+            print(f"X: {mf_comp[0]}; Y: {mf_comp[1]}; Z: {mf_comp[2]}; temperature: {sensor.get_temperature()}, {x}")
+        index += 1
+        if index > 30:
+            break
+
+    print("Continuous meas mode measurement. 8 Milli-Gauss Field Resolution!")
+    sensor.start_measure(continuous_mode=True, update_rate=upd_rate, full_scale=True, over_sample_ratio=3)
+    print(f"Is continuous meas mode: {sensor.is_continuous_meas_mode()}")
+    wt = sensor.get_conversion_cycle_time()
+    delay_func(wt)
+    for mf_comp in sensor:
+        delay_func(wt)
+        if mf_comp:
+            x = math.sqrt(sum(map(lambda val: val ** 2, mf_comp)))  # напряженность магнитного поля в условных ед.
+            print(f"X: {mf_comp[0]}; Y: {mf_comp[1]}; Z: {mf_comp[2]}; temperature: {sensor.get_temperature()}, {x}")
+
+    # while True:
+    #     if sensor.is_data_ready():
+    #         x = sensor.get_meas_result('x')
+    #         y = sensor.get_meas_result('y')
+    #         z = sensor.get_meas_result('z')
+    #         print(x, y, z)
---
+++ qmc5883mod.py
@@ -0,0 +1,119 @@
+"""MicroPython module for QMC5883L or HMC5883L Geomagnetic Sensor"""
+# import struct
+# import micropython
+
+# MicroPython
+# mail: goctaprog@gmail.com
+# MIT license
+from sensor_pack import bus_service, geosensmod
+from sensor_pack.base_sensor import check_value, Iterator, TemperatureSensor
+# import time
+
+_dly_ms = 100, 20, 7, 5
+
+
+class QMC5883L(geosensmod.GeoMagneticSensor, Iterator, TemperatureSensor):
+    """QMC5883L or HMC5883L Geomagnetic Sensor."""
+
+    def __init__(self, adapter: bus_service.BusAdapter, address: int = 0x0D):
+        self._update_rate = 0
+        # адрес 0x0D!
+        check_value(address, (0x0D,), f"Invalid address value: {address}")
+        super().__init__(adapter=adapter, address=address, big_byte_order=False)     # little endian
+        self.setup()
+
+    def _read_reg(self, reg_addr: int, bytes_count: int = 1) -> bytes:
+        """Считывает значение из регистра по адресу регистра 0..0x10. Смотри _get_reg_address"""
+        return self.adapter.read_register(self.address, reg_addr, bytes_count)
+
+    def _write_reg(self, reg_addr: int, value: int, bytes_count: int = 1):
+        """Записывает в регистр с адресом reg_addr значение value по шине."""
+        bo = self._get_byteorder_as_str()[0]
+        self.adapter.write_register(self.address, reg_addr, value, bytes_count, bo)
+
+    def _get_ctrl_1(self) -> int:
+        """возвращает содержимое первого регистра управления"""
+        return self._read_reg(0x09)[0]
+
+    def get_id(self):
+        """Возвращает значение (Chip ID), которое равно 0xFF!"""
+        return self._read_reg(0x0D)[0]
+
+    def get_temperature(self) -> [int, float]:
+        raw = self._read_reg(0x07, 2)
+        return 0.02 * self.unpack("h", raw)[0]  # signed short
+
+    def is_continuous_meas_mode(self):
+        """Возвращает Истина, когда включен режим периодических измерений!"""
+        return 0 != (0x01 & self._get_ctrl_1())
+
+    def in_standby_mode(self) -> bool:
+        """Возвращает Истина, когда включен режим ожидания(экономичный режим)!"""
+        return 0 == (0x01 & self._get_ctrl_1())
+
+    def get_status(self) -> tuple:
+        """Возвращает кортеж битов(номер бита): Data Skip (DOR) (2), Overflow flag (OVL) (1), Data Ready (0)"""
+        stat = self._read_reg(0x06)[0]
+        #           DOR                 OVL                 DRDY
+        return 0 != (stat & 0x04), 0 != (stat & 0x02), 0 != (stat & 0x01)
+
+    def is_data_ready(self) -> bool:
+        """Возвращает флаг Data Ready (DRDY)"""
+        return self.get_status()[2]
+
+    def soft_reset(self):
+        """Выполняет програмный сброс датчика"""
+        val = self._read_reg(0x0A)[0]
+        self._write_reg(0x0A, 0x80 | val)
+
+    def start_measure(self, continuous_mode: bool = True, update_rate: int = 0,
+                      full_scale: bool = False, over_sample_ratio: int = 3):
+        """Запускает периодические измерения (continuous_mode is True) или переводит датчик в
+        режим ожидания (continuous_mode is False).
+        update_rate: 0-10 Hz; 1-50 Hz; 2-100 Hz; 3-200 Hz.  For most of compassing applications, recommend 10 Hz!
+        full_scale: False-2 Gauss; True-8 Gauss;            Field ranges of the magnetic sensor!
+        over_sample_ratio: 0-512; 1-256; 2-128; 3-64.       Larger OSR value leads to smaller filter bandwidth,
+                                                            less in-band noise and higher power consumption."""
+        check_value(update_rate, range(4), f"Invalid update rate: {update_rate}")
+        check_value(over_sample_ratio, range(4), f"Invalid over sample ratio: {update_rate}")
+        ctrl_reg1_val = (over_sample_ratio << 6) | (int(full_scale) << 4) | (update_rate << 2) | int(continuous_mode)
+        self._update_rate = update_rate     # сохраняю
+
+        self._write_reg(0x09, ctrl_reg1_val)
+
+    def read_raw(self, axis_name: int) -> int:
+        addr_reg = geosensmod.axis_name_to_reg_addr(axis_name, offset=0, multiplier=2)
+        bts = self._read_reg(reg_addr=addr_reg, bytes_count=2)   # 16 bit value (int16)
+        return self.unpack('h', bts)[0]
+
+    def _get_all_meas_result(self) -> tuple:
+        bts = self._read_reg(reg_addr=0, bytes_count=6)   # 3x16 bit value (int16)
+        return self.unpack('hhh', bts)
+
+    def get_conversion_cycle_time(self) -> int:
+        """Возвращает время, в микросекундах(!), преобразования датчиком в зависимости от его настроек.
+        Для режима периодических измерений, устанавливает частоту обновления значений величины магнитного поля
+        update_rate должно быть в диапазоне от 0 до 3 включительно, что соответствует частотам:
+        0 - 10 Hz; 1 - 50 Hz; 2 - 150 Hz; 2 - 100 Hz; 3 - 200 Hz"""
+        upd_rate = self._update_rate
+        check_value(upd_rate, range(4), f"Invalid update rate: {upd_rate}")
+        return _dly_ms[upd_rate]
+
+    def setup(self):
+        # roll-over function disabled, INT_ENB: “0”: enable interrupt PIN, “1”: disable interrupt PIN
+        # bit_name      bit number
+        # SOFT_RST      7
+        # ROL_PNT       6
+        # INT_ENB       0
+        self._write_reg(reg_addr=0x0A, value=0x00)
+        # SET/RESET Period. It is recommended that the register 0BH is written by 0x01.
+        self._write_reg(reg_addr=0x0B, value=0x01)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """возвращает результат только в режиме периодических измерений!"""
+        if self.is_continuous_meas_mode and self.is_data_ready():
+            return self.get_axis(-1)
+        return None
